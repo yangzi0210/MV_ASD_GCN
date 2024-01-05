76ef5da2401b1aefd3594f133fa3c59fe2f40e29
yangzi0210 <137907196@qq.com>
2024-01-05 08:36:38 +0800

    feat: 尝试添加注意力机制

--- multiview_model.py
+++ multiview_model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
 
+
 # TODO：添加注意力机制
 class MultiViewGNN(torch.nn.Module):
     def __init__(self, args):
@@ -9,6 +10,7 @@ class MultiViewGNN(torch.nn.Module):
         self.num_features = args.num_features
         self.nhid = args.nhid
         self.dropout_ratio = args.dropout_ratio
+        self.attention_mechanism = SelfAttention(self.nhid)
         self.conv1_v1 = GCNConv(self.num_features, self.nhid)
         self.conv2_v1 = GCNConv(self.nhid, 1)
 
@@ -40,8 +42,34 @@ class MultiViewGNN(torch.nn.Module):
         x_v3 = F.dropout(x_v3, p=self.dropout_ratio, training=self.training)
         x_v3 = self.conv2_v3(x_v3, edge_index_v3)
 
-        # 合并多视图
-        x_multiview = x_v1 + x_v2 + x_v3  # 可以是加和、拼接等
+        # 合并多视图 - 加和
+        x_multiview = x_v1 + x_v2 + x_v3
+        # 合并多视图 - 拼接
+        # x_multiview = torch.cat((x_v1, x_v2, x_v3), dim=1)  # 注意dim=1表示沿特征的维度拼接
+        # 合并多视图 - 自注意力
+        # x_multiview = self.attention_mechanism(x_v1, x_v2, x_v3)
         x = torch.flatten(x_multiview)
+
         features = features_v1 + features_v2 + features_v3
         return x, features
+
+
+class SelfAttention(torch.nn.Module):
+    def __init__(self, nhid):
+        super(SelfAttention, self).__init__()
+        self.query = torch.nn.Linear(nhid, nhid)
+        self.key = torch.nn.Linear(nhid, nhid)
+        self.value = torch.nn.Linear(nhid, nhid)
+        self.softmax = torch.nn.Softmax(dim=1)
+
+    def forward(self, x1, x2, x3):
+        # 计算自注意力得分
+        q1 = self.query(x1)
+        k2 = self.key(x2)
+        v3 = self.value(x3)
+        attention_scores = torch.matmul(q1, k2.transpose(-2, -1))
+        attention_scores = self.softmax(attention_scores)
+
+        # 应用得分到value
+        weighted_values = torch.matmul(attention_scores, v3)
+        return weighted_values
